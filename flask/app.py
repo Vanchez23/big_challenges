1cfbdb9891454e8ca0c30e05fc16d1b6dd211f43
ArtyomKondakov <kondakovartyom@mail.ru>
2021-07-21 17:52:56 +0300

    add model in flask

--- flask/app.py
+++ flask/app.py
@@ -4,7 +4,11 @@ import datetime, time
 import os, sys
 import numpy as np
 from threading import Thread
+import sys
+sys.path.insert(0,'..')
 import mediapipe as mp
+from DeepProtect import Detector
+
 
 global pose_estimation, switch
 switch=1
@@ -15,8 +19,9 @@ pose_estimation=0
 #instatiate flask app  
 app = Flask(__name__, template_folder='./templates')
 API_CAMERA = 'rtsp://admin:camera12345@172.22.103.2/ch1-s1?tcp'
+detector = Detector(path_to_model = '/home/student/model/best.pt')
 
-camera = cv2.VideoCapture(API_CAMERA)
+camera = cv2.VideoCapture(0)
 
 def gen_frames():  # generate frame by frame from camera
     mp_drawing = mp.solutions.drawing_utils
@@ -28,31 +33,42 @@ def gen_frames():  # generate frame by frame from camera
             success, frame = camera.read()
             if success:
                 if(pose_estimation):
-                    # success, frame = camera.read()
-                    # Flip the image horizontally for a later selfie-view display, and convert
-                    # the BGR image to RGB.
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    # To improve performance, optionally mark the image as not writeable to
-                    # pass by reference.
-                    frame.flags.writeable = False
-                    results = pose.process(frame)
+                    # # success, frame = camera.read()
+                    # # Flip the image horizontally for a later selfie-view display, and convert
+                    # # the BGR image to RGB.
+                    #
+                    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    # # To improve performance, optionally mark the image as not writeable to
+                    # # pass by reference.
+                    # frame.flags.writeable = False
+                    # results = pose.process(frame)
+                    #
+                    # # Draw the pose annotation on the image.
+                    # frame.flags.writeable = True
+                    # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+                    # mp_drawing.draw_landmarks(
+                    #     frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
-                    # Draw the pose annotation on the image.
-                    frame.flags.writeable = True
-                    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-                    mp_drawing.draw_landmarks(
-                        frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+                    camera.grab()
+                    res = detector.detect(frame, isDrawing=False)
+                    if isinstance(res, list):
+                        # print(res[0])
+                        frame = res[1]
+                    else:
+                        if res == {'num_people': 1, 'all_wear': True, 'finally': True}:
+                            print('green')
 
                 try:
-                    ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+                    # ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+                    ret, buffer = cv2.imencode('.jpg', frame)
                     frame = buffer.tobytes()
                     yield (b'--frame\r\n'
                            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
                 except Exception as e:
-                    pass
+                    print(e)
 
-            else:
-                pass
+        else:
+            pass
 
 
 @app.route('/')
@@ -81,8 +97,9 @@ def tasks():
                 camera.release()
                 cv2.destroyAllWindows()
 
+
             else:
-                camera = cv2.VideoCapture(API_CAMERA)
+                camera = cv2.VideoCapture(0)
                 switch = 1
 
     elif request.method=='GET':
