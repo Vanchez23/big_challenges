00ab5cd06bd7b41819253295412eae84d9d47095
ArtyomKondakov <kondakovartyom@mail.ru>
2021-07-22 17:31:27 +0300

    rtsp_instead_cv2

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, Response, request
 import cv2
-import time
-
+import rtsp
 import datetime, time
 import os, sys
 import numpy as np
@@ -15,38 +14,33 @@ from DeepProtect import Detector
 global pose_estimation, switch
 switch=1
 pose_estimation=0
-
+is_opened = True
 
 
 #instatiate flask app  
 app = Flask(__name__, template_folder='./templates')
-API_CAMERA = 'rtsp://admin:camera12345@172.22.103.2'
 detector = Detector(path_to_model = '/home/student/model/best.pt')
+client = rtsp.Client(rtsp_server_uri = 'rtsp://admin:camera12345@172.22.103.2', verbose=True)
 
-camera = cv2.VideoCapture(API_CAMERA)
 
 def gen_frames():  # generate frame by frame from camera
-    while camera.isOpened():
-        success, frame = camera.read()
-        # frame = cv2.resize(frame,(640,370))
-        if success:
-            if(pose_estimation):
-                camera.grab()
-                res = detector.detect(frame, isDrawing=True)
-                if isinstance(res, list):
-                    frame = res[1]
-                else:
-                    if res == {'num_people': 1, 'all_wear': True, 'finally': True}:
-                        print('green')
+    while client.isOpened():
+        frame = client.read(raw=True)
+        if(pose_estimation):
+            res = detector.detect(frame, isDrawing=True)
+            if isinstance(res, list):
+                frame = res[1]
+            else:
+                if res == {'num_people': 1, 'all_wear': True, 'finally': True}:
+                    print('green')
 
-            try:
-                # ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
-                ret, buffer = cv2.imencode('.jpg', frame)
-                frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            except Exception as e:
-                print(e)
+        try:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        except Exception as e:
+            print(e)
 
 
 
@@ -61,24 +55,19 @@ def video_feed():
 
 @app.route('/requests',methods=['POST','GET'])
 def tasks():
-    global switch,camera
+    global switch, client, is_opened
     if request.method == 'POST':
         if  request.form.get('pose_estimation') == 'Pose estimation':
             global pose_estimation
             pose_estimation=not pose_estimation
 
-
         elif request.form.get('stop') == 'Stop/Start':
-
             if (switch == 1):
                 switch = 0
-                # print(switch)
-                camera.release()
-                cv2.destroyAllWindows()
-
+                client.close()
 
             else:
-                camera = cv2.VideoCapture(API_CAMERA)
+                client = rtsp.Client(rtsp_server_uri = 'rtsp://admin:camera12345@172.22.103.2',verbose=True)
                 switch = 1
 
     elif request.method=='GET':
