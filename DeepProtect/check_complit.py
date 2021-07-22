bf5966ecf00adc0085f7afcd94f7c04b54d5109b
mikhail0920 <m.ilchen2yandex.ru>
2021-07-22 15:44:07 +0300

    Fixed hand evrictics

--- DeepProtect/check_complit.py
+++ DeepProtect/check_complit.py
@@ -16,24 +16,14 @@ class Wear:
     def distance(self, p1, p2):
         return hypot(p1[0] - p2[0], p1[1] - p2[1])
 
-    def bb_intersection_over_union(self, boxA, boxB):
-        # determine the (x, y)-coordinates of the intersection rectangle
-        xA = max(boxA[0], boxB[0])
-        yA = max(boxA[1], boxB[1])
-        xB = min(boxA[2], boxB[2])
-        yB = min(boxA[3], boxB[3])
-        # compute the area of intersection rectangle
+    def box_intersection(self, box_glove, box_jacket):
+        xA = max(box_glove[0], box_jacket[0])
+        yA = max(box_glove[1], box_jacket[1])
+        xB = min(box_glove[2], box_jacket[2])
+        yB = min(box_glove[3], box_jacket[3])
         interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
-        # compute the area of both the prediction and ground-truth
-        # rectangles
-        boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-        boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
-        # compute the intersection over union by taking the intersection
-        # area and dividing it by the sum of prediction + ground-truth
-        # areas - the interesection area
-        iou = interArea / float(boxAArea + boxBArea - interArea)
-        # return the intersection over union value
-        return iou
+        box_glove_Area = (box_glove[2] - box_glove[0] + 1) * (box_glove[3] - box_glove[1] + 1)
+        return interArea / box_glove_Area
 
     def point_check(self, box, points, name = '', num = 1):
         local_popo = points[name]
@@ -52,16 +42,13 @@ class Wear:
                         coord[1] >= row['y1'] and coord[1] <= row['y2']):
                         mas[ind] = True
                         popusk.append(ind_box)
-                    # else:
-                    #     jacket_df = df[df['label'] == 'jacket']
-                    #     boxA = (row['x1'].tolist(), row['y1'].tolist(), row['x2'].tolist(), row['y2'].tolist())
-                    #     print(boxA)
-                    #     boxB = (jacket_df['x1'].tolist(), jacket_df['y1'].tolist(), jacket_df['x2'].tolist(), jacket_df['y2'].tolist())
-                    #     print(boxB)
-                    #     print(self.bb_intersection_over_union(boxA, boxB))
-                    #print(self.distance(coord, (row['x1'] + (row['x2'] - row['x1']) / 2, row['y1'] + (row['y2'] - row['y1']) / 2)), dist)
-                    # elif self.distance(coord, (row['x1'] + (row['x2'] - row['x1']) / 2, row['y1'] + (row['y2'] - row['y1']) / 2)) <= dist:
-                    #     print('Norm distance')
+                    else:
+                        jacket_df = df[df['label'] == 'jacket'].iloc[0]
+                        boxA = [row['x1'].item(), row['y1'].item(), row['x2'].item(), row['y2'].item()]
+                        boxB = [jacket_df['x1'].item(), jacket_df['y1'].item(), jacket_df['x2'].item(), jacket_df['y2'].item()]
+                        sq = self.box_intersection(boxA, boxB) * 100
+                        if sq > 40:
+                            mas[ind] = True
                 elif row['label'] == name:
                     if (coord[0] >= row['x1'] and coord[0] <= row['x2'] and
                         coord[1] >= row['y1'] and coord[1] <= row['y2']):
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ IS_DRAWING = True
 detector = Detector(path_to_model = '/home/student/Projects/BC_Object_Detection/models/best_Artem2.pt')
 
 #cap = cv2.VideoCapture('rtsp://admin:camera12345@172.22.103.2')
-cap = cv2.VideoCapture('videos/Misha3.mp4')
+cap = cv2.VideoCapture('videos/Misha1.mp4')
 #out = cv2.VideoWriter('videos/outpy.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 10, (1280, 720))
 _, frame = cap.read()
 
@@ -14,22 +14,12 @@ while _:
     orig_img = frame.copy()
     res = detector.detect(frame, isDrawing=IS_DRAWING)
     if isinstance(res, list):
-        #print(res[0])
         frame = res[1]
-    #out.write(res[1])
+
     cv2.imshow('Frame', frame)
     key = cv2.waitKey(20)
     if key == 27:  # exit on ESC
         break
     _, frame = cap.read()
 cap.release()
-# out.release()
 cv2.destroyAllWindows()
-
-# frame = cv2.imread('videos/frame_all_wear.jpg')
-# #frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-# res = detector.detect(frame)
-# print(res[0])
-# cv2.imshow('Frame', res[1])
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
